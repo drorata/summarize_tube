05649b7b6d1959492e959d42b798c57e276197e2
Dror Atariah <drorata@gmail.com>
2025-01-26 21:25:36 +0100

    Added github models based access

--- summarize_tube/settings.py
+++ summarize_tube/settings.py
@@ -5,4 +5,5 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    openai_api_key: str = Field(...)
+    openai_api_key: str | None = Field(None)
+    github_token: str | None = Field(None)
